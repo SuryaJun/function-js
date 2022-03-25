8a14f0f2ec96ae9ba7366ea771ac9db8d8699724
SuryaJun <101086352+SuryaJun@users.noreply.github.com>
2022-03-25 21:44:35 +0700

    revisi script
    perbaikan tugas - game function js

--- scriptfunction.js
+++ scriptfunction.js
@@ -8,26 +8,48 @@ let skorP1 = 0;
 let skorP2 = 0;
 let stop = false;
 
-for(let ronde = 1; ronde <= 5; ronde++) {
-    alert(`ronde` + ronde);
+
+function inputPlayer(player) {
     const rangeNumber = [1, 2, 3];
-    let p1 = prompt('Player 1 : Masukkan angka');
-    let p2 = prompt('Player 2 : Masukkan angka');
-    
-    while(p1 == p2) {
-        alert("tebakan tidak boleh sama");
-        stop = false;
+    let angka = prompt(`player ${player} : Masukkan angka`);
+    while ((rangeNumber.includes(angka))) {
+        alert("angka yang dimasukkan hanya 1-3");
+        angka = prompt(`Player ${player} : masukkan angka`);
     }
-    if(p1 > p2) {
+    return angka;
+}
+
+for(let ronde = 1; ronde <= 5; ronde++) {
+    alert(`ronde ${ronde}`);
+    let angkaBenar = Math.floor(Math.random() * 3) + 1;
+    let p1 = inputPlayer(1);
+    let p2 = inputPlayer(2);    
+
+
+    while (p1 == p2) {
+        alert("tebakan tidak boleh sama");
+        p1 = inputPlayer(1);
+        p2 = inputPlayer(2); 
+    }
+
+    if(p1 == angkaBenar) {
         alert("Good Jobs Player 1");
         skorP1++;
     }
-    else if(p1 < p2) {
+    else if (p2 == angkaBenar) {
         alert("Good Jobs Player 2");
         skorP2++;
     }
-    else if(rangeNumber > 3) {
-        alert("angka tidak boleh lebih dari 3");
+    else {
+        alert("semua tebakan salah");
     }
     
+}
+
+if (skorP1 > skorP2) {
+    alert(`Player 1 menang dengan skor ${skorP1}-${skorP2}`);
+}else if (skorP2 > skorP1) {
+    alert(`Player 2 menang dengan skor ${skorP1}-${skorP2}`);
+}else {
+    alert(`Pertandingan SERI dengan skor ${skorP1}-${skorP2}`);
 }
