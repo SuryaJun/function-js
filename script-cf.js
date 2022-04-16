eaa1cbcb4fced14897777f2bdaf7e18343148e72
SuryaJun <101086352+SuryaJun@users.noreply.github.com>
2022-04-16 21:22:02 +0700

    tgs control flow
    tugas eduwork membuat hasil nilai (alphabet) berdasarkan input nilai (angka)

---
+++ script-cf.js
@@ -0,0 +1,12 @@
+var nilai = prompt("masukkan nilai:");
+if (nilai > 79) {
+  alert("nilai kamu A");
+} else if (nilai > 59) {
+  alert("nilai kamu B");
+} else if (nilai > 39) {
+  alert("nilai kamu C");
+} else if (nilai > 19) {
+  alert("nilai kamu D");
+} else {
+  alert("nilai kamu E");
+}
